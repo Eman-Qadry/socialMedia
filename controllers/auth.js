fd72964e178bbee61462da3a78a2621a28da6170
Eman-Qadry <wafaakadry756@gmail.com>
2024-08-06 19:37:28 +0300

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+
+
+
+exports.register=(req,res,next)=>{
+console.log('not implemented yet!');
+};
+exports.login=(req,res,next)=>{
+    console.log('not implemented yet!');
+};
+
+exports.forgotpassword=(req,res,next)=>{
+    console.log('not implemented yet!');
+};
+
+exports.resetpassword=(req,res,next)=>{
+    console.log('not implemented yet!');
+};
